3a29ea28c048128af15312293a3dfd4d5ed09db2
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-12-02 21:03:21 +0500

    Made hero section responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,57 +1,63 @@
-import React from "react";
 import Image from "next/image";
 import About from "./about";
 
-const Hero = () => {
+export default function Hero() {
   return (
     <>
-      <section className="relative bg-black hero-background h-screen flex items-center">
+      <section className="relative bg-black hero-background h-[100vh] flex items-center">
         <div className="container mx-auto px-10 md:px-16 grid grid-cols-1 md:grid-cols-2 items-center">
-          {/* Left Side ka Content */}
+          {/* Left wala Content */}
           <div className="text-white space-y-6">
             <p className="text-yellow-500 text-xl font-great-vibes">
-              It's Quick & Amazing!
+              It’s Quick & Amazing!
             </p>
 
             <h1 className="text-5xl font-bold">
-              <span className="text-yellow-500">The</span> Art of Speed
-              <br />
+              <span className="text-yellow-500">The</span> Art of Speed <br />
               Food Quality
             </h1>
 
-            {/* Right side ka content */}
-            <div className="relative">
-              <Image
-                src="/hero/heropic.svg"
-                alt="Hero Image"
-                width={500}
-                height={500}
-                className="relative z-10"
-              />
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius
+              sed pharetra dictum neque massa congue.
+            </p>
 
-              <div className="absolute top-1/4 left-[20%] w-16 h-16">
-                <Image
-                  src="/path-to-circle-item-1.svg"
-                  alt="Item 1"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="absolute top-1/3 right-[15%] w-12 h-12">
-                <Image
-                  src="/path-to-circle-item-2.svg"
-                  alt="Item 2"
-                  width={48}
-                  height={48}
-                />
-              </div>
+            <button className="bg-yellow-500 text-black font-medium py-3 px-6 rounded-full shadow-md hover:bg-yellow-600 transition">
+              See Menu
+            </button>
+          </div>
+
+          {/* Right wala Content */}
+          <div className="relative">
+            <Image
+              src="/hero/heropic.svg"
+              alt="Hero Image"
+              width={500}
+              height={500}
+              className="relative z-10"
+            />
+            <div className="absolute top-1/4 left-[20%] w-16 h-16">
+              <Image
+                src="/path-to-circle-item-1.svg"
+                alt="Item 1"
+                width={64}
+                height={64}
+              />
+            </div>
+            <div className="absolute top-1/3 right-[15%] w-12 h-12">
+              <Image
+                src="/path-to-circle-item-2.svg"
+                alt="Item 2"
+                width={48}
+                height={48}
+              />
             </div>
           </div>
         </div>
       </section>
-      <About/>
+
+      {/* Rest sections will put them here hehe */}
+      <About />
     </>
   );
-};
-
-export default Hero;
+}
