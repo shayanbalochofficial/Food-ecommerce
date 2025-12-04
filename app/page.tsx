cb21881effdfb15fc4926cb3a9fd8ee2373d9145
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-12-04 20:42:14 +0500

    Created Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import HeroSection from "./../components/sections/HeroSection";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutSection from "@/components/sections/AboutSection";
+import FoodCategorySection from "@/components/sections/FoodCategorySection";
 
 const page = () => {
   return (
     <>
       <HeroSection />
+      <AboutSection />
+      <FoodCategorySection />
     </>
   );
 };
